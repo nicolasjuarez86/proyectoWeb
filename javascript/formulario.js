65c8e864a56c5bacf16b5a34e3cf8e7d7712f70d
Nicolas Juarez <nicolas.juarez86@gmail.com>
2023-05-13 18:13:32 -0300

    =agregada la validación de formularios, vista full de cada producto, agregado todos los html, falta agregar los datos

---
+++ javascript/formulario.js
@@ -0,0 +1,75 @@
+const formulario = document.getElementById('formulario');
+
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+
+const campos = {
+	nombre: false,
+	correo: false,
+	telefono: false
+}
+
+const validarFormulario = (e) => {
+	switch (e.target.name) {
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'nombre');
+		break;
+		case "correo":
+			validarCampo(expresiones.correo, e.target, 'correo');
+		break;
+		case "telefono":
+			validarCampo(expresiones.telefono, e.target, 'telefono');
+		break;
+	}
+}
+
+const validarCampo = (expresion, input, campo) => {
+	console.log(`attribute-${campo}`);
+	if(expresion.test(input.value)){
+		document.getElementById(`attribute-${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`attribute-${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#attribute-${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#attribute-${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#attribute-${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`attribute-${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`attribute-${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#attribute-${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#attribute-${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#attribute-${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+	}
+}
+
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);
+	input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const terminos = document.getElementById('terminos');
+	if(campos.nombre && campos.correo && campos.telefono && terminos.checked ){
+		formulario.reset();
+
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		setTimeout(() => {
+			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		}, 5000);
+
+		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correcto');
+		});
+	} else {
+		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+	}
+});
--- javascript/main.js
+++ javascript/main.js
@@ -24,6 +24,41 @@ $(document).ready(function(){
   });
 });
 
+/* TAB PANEL */
+
+$(document).ready(function(){
+
+  $('.tab-panel').each(function(){
+
+      tabChange($('ul.tabs a', this).first().attr('href'));
+
+      $('.tabs a').on('click', function(){
+          if(history.replaceState){
+              history.replaceState({}, '', $(this).attr('href'));
+              tabChange($(this).attr('href'));
+          } else
+              location.hash = $(this).attr('href');
+          return false;
+      });
+  });
+  function tabChange(hash){
+      var tab = hash.substr(1);
+
+      if(!tab) return;
+
+      $(".tab-content").hide();
+      $(".tab-content[tab='"+tab+"']").show();
+
+      $("ul.tabs a").removeClass('selected');
+      $("ul.tabs a[href='#"+tab+"']").addClass('selected');
+
+  }
+  $(window).on('hashchange', function(){
+      tabChange(location.hash);
+  });
+  tabChange(location.hash);
+
+});
 
 
 
