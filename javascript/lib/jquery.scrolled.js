dc589464a54fadc1186bf8e37f12e6c72fb4ba60
Nicolas Juarez <nicolas.juarez86@gmail.com>
2023-05-14 22:21:45 -0300

    responsive todas las secciones, agregado selected menu ppal arreglado contacto y nosotros

--- javascript/lib/jquery.scrolled.js
+++ javascript/lib/jquery.scrolled.js
@@ -1,3 +1,16 @@
+
+/* SCROLLED */
+
+$(document).ready(function(){
+    var HEADER_HEIGHT = 170;
+    var HEADER_HEIGHT_SCROLLED = 90;
+  
+    $('.Header').scrolled({
+      scroll: HEADER_HEIGHT - HEADER_HEIGHT_SCROLLED
+    });
+  });
+  
+
 (function($){
     $.fn.scrolled = function(options) {
 
